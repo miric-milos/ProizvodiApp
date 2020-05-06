e26f9aaf599b841bf969c3ebad9c719b859e2cc1
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2020-05-06 20:03:18 +0200

    Kreirana baza podataka

---
+++ ASPProizvodiApp/Migrations/202005061748578_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿namespace ASPProizvodiApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Proizvodi",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Naziv = c.String(),
+                        Opis = c.String(),
+                        Kategorija = c.String(),
+                        Proizvodjac = c.String(),
+                        Dobavljac = c.String(),
+                        Cena = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ProizvodI");
+        }
+    }
+}
