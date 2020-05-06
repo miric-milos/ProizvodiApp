422b23248feab2ecfd2664bc51ade1deb32276d4
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2020-05-06 19:38:03 +0200

    Add project files.

---
+++ ASPProizvodiApp/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASPProizvodiApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Omogucava koriscenje Route atributa u kontroleru
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ASPProizvodiApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPProizvodiApp.MvcApplication" Language="C#" %>
