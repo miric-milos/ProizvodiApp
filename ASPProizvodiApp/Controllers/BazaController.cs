6d4e1327f0d135f2641fd0416a724a754378f802
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2020-05-08 17:16:58 +0200

    Omoguceno izmenjivanje proizvoda, zavrsen deo vezan za bazu

--- ASPProizvodiApp/Controllers/BazaController.cs
+++ ASPProizvodiApp/Controllers/BazaController.cs
@@ -44,13 +44,68 @@ namespace ASPProizvodiApp.Controllers
                     Cena = model.Cena
                 };
 
-                if(await ProizvodManager.DodajProizvodAsync(proizvod))
+                if (await ProizvodManager.DodajProizvodAsync(proizvod))
                 {
                     return Redirect("/db/proizvodi");
                 }
                 // Nije uspelo dodavanje
                 ModelState.AddModelError("", "Doslo je do greske pri dodavanju.");
                 return View(model);
+            }
+            // Model nije u redu
+            return View(model);
+        }
+
+        [Route("proizvodi/{id}")]
+        public ActionResult Izmeni(int id = -1)
+        {
+            var proizvod = ProizvodManager.GetProizvod(id);
+
+            if (proizvod != null)
+            {
+                // Koriscenjem TempData ne izlazemo id korisniku
+                TempData["id"] = proizvod.Id;
+                return View(new ProizvodViewModel 
+                {
+                    Naziv = proizvod.Naziv,
+                    Opis = proizvod.Opis,
+                    Kategorija = proizvod.Kategorija,
+                    Proizvodjac = proizvod.Proizvodjac,
+                    Dobavljac = proizvod.Dobavljac,
+                    Cena = proizvod.Cena
+                });
+            }
+            // Ne otkrivati da proizvod ne postoji
+            return View("Greska");
+        }
+
+        [HttpPost]
+        [Route("proizvodi/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Izmeni(ProizvodViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var proizvod = ProizvodManager.GetProizvod((int)TempData["id"]);
+                if (proizvod != null)
+                {
+                    proizvod.Naziv = model.Naziv;
+                    proizvod.Opis = model.Opis;
+                    proizvod.Kategorija = model.Kategorija;
+                    proizvod.Proizvodjac = model.Proizvodjac;
+                    proizvod.Dobavljac = model.Dobavljac;
+                    proizvod.Cena = model.Cena;
+
+                    if(await ProizvodManager.IzmeniProizvodAsync(proizvod))
+                    {
+                        return Redirect("/db/proizvodi");
+                    }
+                    // Greska pri izmeni
+                    ModelState.AddModelError("", "Doslo je do greske pri izmeni");
+                    return View(model);
+                }
+                // Proizvod nije pronadjen
+                return View("Greska");
             }
             // Model nije u redu
             return View(model);
--- ASPProizvodiApp/Models/Infrastructure/ProizvodManager.cs
+++ ASPProizvodiApp/Models/Infrastructure/ProizvodManager.cs
@@ -1,6 +1,7 @@
 ﻿using Domain;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -37,6 +38,36 @@ namespace ASPProizvodiApp.Models.Infrastructure
                 }
             }
             return true;
+        }
+
+        public static Proizvod GetProizvod(int id)
+        {
+            using (var db = new ProizvodiContext())
+            {
+                var lista = db.Proizvodi.Where(p => p.Id == id);
+                return lista.Count() != 1 ? null : lista.FirstOrDefault();
+            }
+        }
+
+        public async static Task<bool> IzmeniProizvodAsync(Proizvod proizvod)
+        {
+            try
+            {
+                using (var db = new ProizvodiContext())
+                {
+                    // var stariProizvod = db.Proizvodi.SingleOrDefault(p => p.Id == proizvod.Id);
+                    db.Proizvodi.AddOrUpdate(proizvod);
+                    await db.SaveChangesAsync();
+
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                // Odkomentarisi throw ako dodje do greske
+                // throw;
+                return false;
+            }       
         }
     }
 }
