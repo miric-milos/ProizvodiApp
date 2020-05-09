c8a4bf35169c95c5429686faf3add5d1c447fe43
Miloš Mirić <56596844+miric-milos@users.noreply.github.com>
2020-05-09 14:04:11 +0200

    Omoguceno dodavanje proizvoda u JSON fajl

---
+++ ASPProizvodiApp/Controllers/JsonController.cs
@@ -0,0 +1,57 @@
+﻿using ASPProizvodiApp.Models.Infrastructure;
+using ASPProizvodiApp.Models.ViewModels;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPProizvodiApp.Controllers
+{
+    [RoutePrefix(prefix: "api")]
+    public class JsonController : Controller
+    {
+        [Route("proizvodi")]
+        public ActionResult Index()
+        {
+            var proizvodi = JsonProizvodiManager.SviProizvodi();
+            return View(proizvodi);
+        }
+
+        [Route("dodaj")]
+        public ActionResult Dodaj()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("dodaj")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Dodaj(ProizvodViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var proizvod = new Proizvod
+                {
+                    Naziv = model.Naziv,
+                    Opis = model.Opis,
+                    Kategorija = model.Kategorija,
+                    Proizvodjac = model.Proizvodjac,
+                    Dobavljac = model.Dobavljac,
+                    Cena = model.Cena
+                };
+
+                if (JsonProizvodiManager.DodajProizvod(proizvod))
+                {
+                    return Redirect("/api/proizvodi");
+                }
+                // Nije uspelo dodavanje
+                ModelState.AddModelError("", "Doslo je do greske pri dodavanju.");
+                return View(model);
+            }
+            // Model nije u redu
+            return View(model);
+        }
+    }
+}
---
+++ ASPProizvodiApp/Models/Infrastructure/JsonProizvodiManager.cs
@@ -0,0 +1,55 @@
+﻿using Domain;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ASPProizvodiApp.Models.Infrastructure
+{
+    public static class JsonProizvodiManager
+    {
+        private static string putanja =
+            HttpContext.Current.Server.MapPath("~/Content/proizvodi.json");
+
+        public static IEnumerable<Proizvod> SviProizvodi()
+        {
+            if (File.Exists(putanja))
+            {
+                var sadrzaj = File.ReadAllText(putanja);
+                var proizvodi = JsonConvert.DeserializeObject<IEnumerable<Proizvod>>(sadrzaj);
+                if (proizvodi != null)
+                {
+                    return proizvodi;
+                }
+            }
+            // Ne postoji fajl ili je prazan
+            return Enumerable.Empty<Proizvod>();
+        }
+
+        public static bool DodajProizvod(Proizvod proizvod)
+        {
+            try
+            {
+                var proizvodi = SviProizvodi().ToList();
+                using (var writer = File.CreateText(putanja))
+                {
+                    proizvod.Id =
+                        proizvodi.Count() == 0 ? 0 : proizvodi[proizvodi.Count() - 1].Id + 1;
+
+                    proizvodi.Add(proizvod);
+                    new JsonSerializer().Serialize(writer, proizvodi);
+
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                // throw;                
+                return false;
+            }
+        }
+    }
+}
